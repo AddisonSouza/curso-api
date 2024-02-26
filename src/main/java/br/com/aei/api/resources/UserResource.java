d79d47abe2472895ead6455aecd3ea606302e484
AddisonSouza <info2019.20193009268@gmail.com>
2024-02-26 13:51:50 -0300

    classe user resource findById

--- src/main/java/br/com/aei/api/resources/UserResource.java
+++ src/main/java/br/com/aei/api/resources/UserResource.java
@@ -1,6 +1,6 @@
 package br.com.aei.api.resources;
 
-import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,14 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.aei.api.domain.User;
+import br.com.aei.api.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
     
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
 
-        return ResponseEntity.ok().body(new User(id, "Addison", "addi@email.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
