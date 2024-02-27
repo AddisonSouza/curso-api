02532d0e1c36a5501e6b6b0ed602bc2e3a90414e
AddisonSouza <info2019.20193009268@gmail.com>
2024-02-27 10:37:38 -0300

    retornando dto para users resource

---
+++ src/main/java/br/com/aei/api/resources/UsersResource.java
@@ -0,0 +1,30 @@
+package br.com.aei.api.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.modelmapper.ModelMapper;
+import br.com.aei.api.domain.Users;
+import br.com.aei.api.domain.dto.UsersDTO;
+import br.com.aei.api.services.UsersService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UsersResource {
+    
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private UsersService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UsersDTO> findById(@PathVariable Integer id) {
+
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UsersDTO.class));
+    }
+}
