ed0693f7afbf8127d7cb3e2f46d6f241535d11cf
AddisonSouza <info2019.20193009268@gmail.com>
2024-03-04 14:07:31 -0300

    teste para endpoint delete da classe user resource implementado

--- src/test/java/br/com/aei/api/resources/UsersResourceTest.java
+++ src/test/java/br/com/aei/api/resources/UsersResourceTest.java
@@ -14,6 +14,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -124,8 +127,15 @@ public class UsersResourceTest {
     }
 
     @Test
-    void testDelete() {
+    void whenDeleteThenReturnSucess() {
+        doNothing().when(service).delete(anyInt());
 
+        ResponseEntity<UsersDTO> response = resource.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        verify(service, times(1)).delete(anyInt());
     }
 
 
