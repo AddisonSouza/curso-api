32d8c86bd8b8d55c6b12b0c728c6b8879f6e5e9e
AddisonSouza <info2019.20193009268@gmail.com>
2024-03-04 10:46:04 -0300

    teste para endpoint findById implementado para classe user resource

--- src/test/java/br/com/aei/api/resources/UsersResourceTest.java
+++ src/test/java/br/com/aei/api/resources/UsersResourceTest.java
@@ -1,47 +1,74 @@
 package br.com.aei.api.resources;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 import br.com.aei.api.domain.Users;
 import br.com.aei.api.domain.dto.UsersDTO;
 import br.com.aei.api.repositories.UsersRepository;
+import br.com.aei.api.services.impl.UsersServiceImpl;
 
 
 @SpringBootTest
 public class UsersResourceTest {
     
-    private static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema";
-    private static final int INDEX = 0;
-    private static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    private static final int INDEX       = 0;
     private static final String PASSWORD = "123";
     private static final String EMAIL    = "valdir@email.com";
     private static final String NAME     = "valdir";
     private static final Integer ID      = 1;
-
-    @InjectMocks    
-    private UsersResource service;
-
-    @Mock
-    private UsersRepository repository;
-
-    @Mock
-    private ModelMapper mapper;
+    private static final String OBJETO_NAO_ENCONTRADO           = "Objeto não encontrado";
+    private static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema";
 
     private Users users;
     private UsersDTO usersDTO;
-    private Optional<Users> optionalUser;
+
+    @InjectMocks    
+    private UsersResource resource;
+
+    @Mock
+    private UsersServiceImpl service;
+
+    @Mock
+    private ModelMapper mapper;
 
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
         startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnSuccess() {
+        when(service.findById(anyInt())).thenReturn(users);
+        when(mapper.map(any(), any())).thenReturn(usersDTO);
+
+        ResponseEntity<UsersDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UsersDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
     }
 
     @Test
@@ -60,11 +87,6 @@ public class UsersResourceTest {
     }
 
     @Test
-    void testFindById() {
-
-    }
-
-    @Test
     void testUpdate() {
 
     }
@@ -72,6 +94,5 @@ public class UsersResourceTest {
      private void startUser() {
         users = new Users(ID, NAME, EMAIL, PASSWORD);
         usersDTO = new UsersDTO(ID, NAME, EMAIL, PASSWORD);
-        optionalUser = Optional.of(new Users(ID, NAME, EMAIL, PASSWORD)); 
     }
 }
