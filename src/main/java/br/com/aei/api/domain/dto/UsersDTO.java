22654490f99e4fc7c776cce71e0b3ede8b52f13e
AddisonSouza <info2019.20193009268@gmail.com>
2024-03-05 11:00:14 -0300

    corrigindo bug noargscontructor

--- src/main/java/br/com/aei/api/domain/dto/UsersDTO.java
+++ src/main/java/br/com/aei/api/domain/dto/UsersDTO.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UsersDTO {
     private Integer id;
     private String name;
